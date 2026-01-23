e55d138d2071301fdbd8bc5304f05b5623645a53
Melo <lucabaudo9412@gmail.com>
2026-01-23 16:35:57 +0100

    First commit, created JSON server

---
+++ BlogAPI/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <header className='title-header'>
+        React Blog
+      </header>
+    </>
+  )
+}
+
+export default App
